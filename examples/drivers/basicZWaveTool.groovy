5ab114ad85a5645f3c5d9524b9d8024fdac9c9d1
Mike Maxwell <MikeMaxwell@users.noreply.github.com>
2018-08-25 08:48:59 -0700

    Update basicZWaveTool.groovy

--- examples/drivers/basicZWaveTool.groovy
+++ examples/drivers/basicZWaveTool.groovy
@@ -14,8 +14,8 @@ metadata {
     definition (name: "Basic Z-Wave tool",namespace: "hubitat", author: "Mike Maxwell") {
         
     	command "getCommandClassReport"
-		command "getParameterReport", ["NUMBER"]
-		command "setParameter", ["NUMBER","NUMBER","NUMBER"]
+	command "getParameterReport", ["NUMBER"]
+	command "setParameter", ["NUMBER","NUMBER","NUMBER"]
     }
 }
 
@@ -30,61 +30,62 @@ def zwaveEvent(hubitat.zwave.commands.configurationv1.ConfigurationReport cmd) {
     def param = cmd.parameterNumber
     def value = cmd.scaledConfigurationValue
     def size = cmd.size
-	log.info "ConfigurationReport- parameterNumber:${param}, size:${size}, value:${value}"
+    log.info "ConfigurationReport- parameterNumber:${param}, size:${size}, value:${value}"
 }
 
 def zwaveEvent(hubitat.zwave.commands.versionv1.VersionCommandClassReport cmd) {
-	def version = cmd.commandClassVersion
-	def cmdClass = cmd.requestedCommandClass
-	log.info "CommandClassReport- class:${ "0x${intToHexStr(cmdClass)}" }, version:${version}"		
+    def version = cmd.commandClassVersion
+    def cmdClass = cmd.requestedCommandClass
+    log.info "CommandClassReport- class:${ "0x${intToHexStr(cmdClass)}" }, version:${version}"		
 }	
 
 def zwaveEvent(hubitat.zwave.commands.securityv1.SecurityMessageEncapsulation cmd) {
-	def encapCmd = cmd.encapsulatedCommand()
-	def result = []
-	if (encapCmd) {
-		result += zwaveEvent(encapCmd)
-	} else {
+    def encapCmd = cmd.encapsulatedCommand()
+    def result = []
+    if (encapCmd) {
+	result += zwaveEvent(encapCmd)
+    } else {
         log.warn "Unable to extract encapsulated cmd from ${cmd}"
-	}
-	return result
+    }
+    return result
 }
 
 def zwaveEvent(hubitat.zwave.Command cmd) {
-	log.debug "skip: ${cmd}"
+    log.debug "skip: ${cmd}"
 }
+
 //cmds
 def setParameter(parameterNumber = null, size = null, value = null){
-	if (parameterNumber == null || size == null || value == null) {
-		log.warn "incomplete parameter list supplied..."
-		log.info "syntax: setParameter(parameterNumber,size,value)"
-	} else {
-		return delayBetween([
-			secureCmd(zwave.configurationV1.configurationSet(scaledConfigurationValue: value, parameterNumber: parameterNumber, size: size)),
-			secureCmd(zwave.configurationV1.configurationGet(parameterNumber: parameterNumber))
-		],500)
-	}
+    if (parameterNumber == null || size == null || value == null) {
+	log.warn "incomplete parameter list supplied..."
+	log.info "syntax: setParameter(parameterNumber,size,value)"
+    } else {
+	return delayBetween([
+	    secureCmd(zwave.configurationV1.configurationSet(scaledConfigurationValue: value, parameterNumber: parameterNumber, size: size)),
+	    secureCmd(zwave.configurationV1.configurationGet(parameterNumber: parameterNumber))
+	],500)
+    }
 }
 
 def getParameterReport(param = null){
-	def cmds = []
-	if (param) {
-		cmds = [secureCmd(zwave.configurationV1.configurationGet(parameterNumber: param))]
-	} else {
-		0.upto(255, {
-			cmds.add(secureCmd(zwave.configurationV1.configurationGet(parameterNumber: it)))	
-		})
-	}
-	return cmds
+    def cmds = []
+    if (param) {
+	cmds = [secureCmd(zwave.configurationV1.configurationGet(parameterNumber: param))]
+    } else {
+	0.upto(255, {
+	    cmds.add(secureCmd(zwave.configurationV1.configurationGet(parameterNumber: it)))	
+	})
+    }
+    return cmds
 }	
 
 def getCommandClassReport(){
-	def cmds = []
-	def ic = getDataValue("inClusters").split(",").collect{ hexStrToUnsignedInt(it) }
-	ic.each {
-		if (it) cmds.add(secureCmd(zwave.versionV1.versionCommandClassGet(requestedCommandClass:it)))
-	}
-	return delayBetween(cmds,500)
+    def cmds = []
+    def ic = getDataValue("inClusters").split(",").collect{ hexStrToUnsignedInt(it) }
+    ic.each {
+	if (it) cmds.add(secureCmd(zwave.versionV1.versionCommandClassGet(requestedCommandClass:it)))
+    }
+    return delayBetween(cmds,500)
 }
 
 def installed(){}
@@ -94,11 +95,11 @@ def configure() {}
 def updated() {}
 
 private secureCmd(cmd) {
-	if (getDataValue("zwaveSecurePairingComplete") == "true") {
-		return zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
-	} else {
-		return cmd.format()
-	}	
+    if (getDataValue("zwaveSecurePairingComplete") == "true") {
+	return zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
+    } else {
+	return cmd.format()
+    }	
 }
 
 
