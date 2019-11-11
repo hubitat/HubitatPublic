3cd071395fdbb949d1d68f1527b94eb3a274973b
bravenel <bravenel@users.noreply.github.com>
2019-11-11 08:49:12 -0700

    Initial commit

---
+++ examples/drivers/virtualOpenVentArea.groovy
@@ -0,0 +1,30 @@
+metadata {
+    definition (name: "Virtual Open Vent Areal", namespace: "hubitat", author: "Bruce Ravenel") {
+        capability "Sensor"
+        command "setOpenVentArea", ["NUMBER"]
+        attribute "openVentArea", "Number"
+
+    }
+    preferences {
+        input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
+    }
+}
+
+def installed() {
+    log.warn "installed..."
+    setOpenVentArea(0)
+}
+
+def updated() {
+    log.info "updated..."
+    log.warn "description logging is: ${txtEnable == true}"
+}
+
+def parse(String description) {
+}
+
+def setOpenVentArea(area) {
+    def descriptionText = "${device.displayName} was set to $area"
+    if (txtEnable) log.info "${descriptionText}"
+    sendEvent(name: "openVentArea", value: area, unit: "sq. m", descriptionText: descriptionText)
+}
