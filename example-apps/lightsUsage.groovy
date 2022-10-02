9a3b629060285a617b512895263f5b281beeee66
bravenel <bravenel@users.noreply.github.com>
2022-10-02 15:01:55 -0700

    Add variables

--- example-apps/lightsUsage.groovy
+++ example-apps/lightsUsage.groovy
@@ -27,7 +27,7 @@ def mainPage() {
 			input "lights", "capability.switch", title: "Select Lights to Measure Usage", multiple: true, submitOnChange: true, width: 4
 			lights.each {dev ->
 				if(!state.lights["$dev.id"]) {
-					state.lights["$dev.id"] = [start: dev.currentSwitch == "on" ? now() : 0, total: 0]
+					state.lights["$dev.id"] = [start: dev.currentSwitch == "on" ? now() : 0, total: 0, var: ""]
 					state.lightsList += dev.id
 				}
 			}
@@ -40,6 +40,20 @@ def mainPage() {
 				}
 				updated()
 				paragraph displayTable()
+				if(state.newVar) {
+					List vars = getAllGlobalVars().findAll{it.value.type == "string"}.keySet().collect().sort{it.capitalize()}
+					input "newVar", "enum", title: "Select Variable", submitOnChange: true, width: 4, options: vars, newLineAfter: true
+					if(newVar) {
+						state.lights[state.newVar].var = newVar
+						state.remove("newVar")
+						app.removeSetting("newVar")
+						paragraph "<script>{changeSubmit(this)}</script>"
+					}
+				} else if(state.remVar) {
+					state.lights[state.remVar].var = ""
+					state.remove("remVar")
+					paragraph "<script>{changeSubmit(this)}</script>"
+				}
 				input "refresh", "button", title: "Refresh Table", width: 2
 				input "reset", "button", title: "Reset Table", width: 2
 			}
@@ -57,18 +71,26 @@ String displayTable() {
 	String str = "<script src='https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js'></script>"
 	str += "<style>.mdl-data-table tbody tr:hover{background-color:inherit} .tstat-col td,.tstat-col th { padding:8px 8px;text-align:center;font-size:12px} .tstat-col td {font-size:15px }" +
 		"</style><div style='overflow-x:auto'><table class='mdl-data-table tstat-col' style=';border:2px solid black'>" +
-		"<thead><tr style='border-bottom:2px solid black'><th style='border-right:2px solid black'>Light</th><th>Total On Time</th><th>Reset</th></tr></thead>"
+		"<thead><tr style='border-bottom:2px solid black'><th style='border-right:2px solid black'>Light</th>" +
+		"<th>Total On Time</th>" +
+		"<th>Reset</th>" +
+		"<th>Variable</th></tr></thead>"
 	lights.sort{it.displayName.toLowerCase()}.each {dev ->
 		int total = state.lights["$dev.id"].total / 1000
+		String thisVar = state.lights["$dev.id"].var
 		int hours = total / 3600
 		total = total % 3600
 		int mins = total / 60
 		int secs = total % 60
+		String time = "$hours:${mins < 10 ? "0" : ""}$mins:${secs < 10 ? "0" : ""}$secs"
+		if(thisVar) setGlobalVar(thisVar, time)
 		String devLink = "<a href='/device/edit/$dev.id' target='_blank' title='Open Device Page for $dev'>$dev"
 		String reset = buttonLink("d$dev.id", "<iconify-icon icon='bx:reset'></iconify-icon>", "black", "20px")
+		String var = thisVar ? buttonLink("r$dev.id", thisVar, "purple") : buttonLink("n$dev.id", "Select", "green")
 		str += "<tr style='color:black'><td style='border-right:2px solid black'>$devLink</td>" +
-			"<td style='color:${dev.currentSwitch == "on" ? "green" : "red"}'>$hours:${mins < 10 ? "0" : ""}$mins:${secs < 10 ? "0" : ""}$secs</td>" +
-			"<td title='Reset Total for $dev' style='padding:0px 0px'>$reset</td></tr>"
+			"<td style='color:${dev.currentSwitch == "on" ? "green" : "red"}'>$time</td>" +
+			"<td title='Reset Total for $dev' style='padding:0px 0px'>$reset</td>" +
+			"<td>$var</td></tr>"
 	}
 	str += "</table></div>"
 	str
@@ -89,7 +111,9 @@ void appButtonHandler(btn) {
 			state.lights[k].total += now() - state.lights[k].start
 			state.lights[k].start = now()
 		}
-	} else state.reset = btn.minus("d")
+	} else if(btn.startsWith("n")) state.newVar = btn.minus("n")
+	else if(btn.startsWith("r")) state.remVar = btn.minus("r")
+	else state.reset = btn.minus("d")
 }
 
 def updated() {
@@ -111,4 +135,13 @@ void onHandler(evt) {
 
 void offHandler(evt) {
 	state.lights[evt.device.id].total += now() - state.lights[evt.device.id].start
+	String thisVar = state.lights[evt.device.id].var
+	if(thisVar) {
+		int total = state.lights[evt.device.id].total / 1000
+		int hours = total / 3600
+		total = total % 3600
+		int mins = total / 60
+		int secs = total % 60
+		setGlobalVar(thisVar, "$hours:${mins < 10 ? "0" : ""}$mins:${secs < 10 ? "0" : ""}$secs")
+	}
 }
